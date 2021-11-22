65c481fa7ab57f20f287af82d8555c91c62dac3b
Oskar Słyk <oskar.slyk@mavoco.com>
2021-11-22 23:03:40 +0100

    firestore connection

--- app/src/main/java/com/oskarro/queue/MainActivity.kt
+++ app/src/main/java/com/oskarro/queue/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.oskarro.queue
 
 import android.os.Bundle
+import android.util.Log
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
@@ -15,22 +16,54 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityMainBinding
+    val TAG = "MyMessage"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        uploadData()
     }
 
-    fun convertText(view: View) {
-        if (dollarText.text.isNotEmpty()) {
-            val dollarValue = dollarText.text.toString().toFloat()
-            val euroValue = dollarValue * 0.85f
-            textView.text = euroValue.toString()
+    private fun uploadData() {
+        binding!!.btnUploadData.setOnClickListener {
+
+            // create a dummy data
+            val hashMap = hashMapOf<String, Any>(
+                "code" to "XC12",
+                "description" to "some information about process",
+                "amount" to 10,
+                "stage" to "release"
+            )
+
+            // use the add() method to create a document inside users collection
+            FirebaseUtils().fireStoreDatabase.collection("processes")
+                .add(hashMap)
+                .addOnSuccessListener {
+                    Log.d(TAG, "Added document with ID ${it.id}")
+                }
+                .addOnFailureListener { exception ->
+                    Log.w(TAG, "Error adding document $exception")
+                }
         }
     }
+
+    private fun readData() {
+        binding!!.btnReadData.setOnClickListener {
+            FirebaseUtils().fireStoreDatabase.collection("users")
+                .get()
+                .addOnSuccessListener { querySnapshot ->
+                    querySnapshot.forEach { document ->
+                        Log.d(TAG, "Read document with ID ${document.id}")
+                    }
+                }
+                .addOnFailureListener { exception ->
+                    Log.w(TAG, "Error getting documents $exception")
+                }
+        }
+    }
+
 
 }
