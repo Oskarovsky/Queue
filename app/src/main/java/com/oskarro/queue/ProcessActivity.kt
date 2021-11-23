35bbc051056d7a93c31c0139c3b8fe2949a042ed
Oskar Słyk <oskar.slyk@mavoco.com>
2021-11-23 20:00:33 +0100

    Splash Activity

--- app/src/main/java/com/oskarro/queue/ProcessActivity.kt
+++
@@ -1,4 +0,0 @@
-package com.oskarro.queue
-
-class ProcessActivity {
-}
---
+++ app/src/main/java/com/oskarro/queue/SplashActivity.kt
@@ -0,0 +1,19 @@
+package com.oskarro.queue
+
+import android.os.Bundle
+import android.view.WindowManager
+import androidx.appcompat.app.AppCompatActivity
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+
+    }
+}
