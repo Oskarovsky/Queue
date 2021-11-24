be6ff9782e387fa8f9a6ff85cf24f5e09e7166ae
Oskar Słyk <oskar.slyk@mavoco.com>
2021-11-24 18:43:57 +0100

    Data class for user

--- app/src/main/java/com/oskarro/queue/firebase/FirebaseUtils.kt
+++ app/src/main/java/com/oskarro/queue/firebase/FirebaseUtils.kt
@@ -1,7 +1,15 @@
 package com.oskarro.queue.firebase
 
 import com.google.firebase.firestore.FirebaseFirestore
+import com.oskarro.queue.activities.SignUpActivity
+import com.oskarro.queue.model.User
 
 class FirebaseUtils {
     val fireStoreDatabase = FirebaseFirestore.getInstance()
+
+    fun registerUser(activity: SignUpActivity, userInfo: User) {
+
+
+
+    }
 }
---
+++ app/src/main/java/com/oskarro/queue/model/User.kt
@@ -0,0 +1,43 @@
+package com.oskarro.queue.model
+
+import android.os.Parcel
+import android.os.Parcelable
+
+data class User (
+    val id: String = "",
+    val name: String = "",
+    val email: String = "",
+    val position: String = "",
+    val mobile: Long = 0,
+    val fcmToken: String = ""
+): Parcelable {
+    constructor(parcel: Parcel) : this(
+        parcel.readString()!!,
+        parcel.readString()!!,
+        parcel.readString()!!,
+        parcel.readString()!!,
+        parcel.readLong(),
+        parcel.readString()!!
+    )
+
+    override fun writeToParcel(dest: Parcel, flags: Int) = with(dest) {
+        writeString(id)
+        writeString(name)
+        writeString(email)
+        writeString(position)
+        writeLong(mobile)
+        writeString(fcmToken)
+    }
+
+    override fun describeContents(): Int = 0
+
+    companion object CREATOR : Parcelable.Creator<User> {
+        override fun createFromParcel(parcel: Parcel): User {
+            return User(parcel)
+        }
+
+        override fun newArray(size: Int): Array<User?> {
+            return arrayOfNulls(size)
+        }
+    }
+}
