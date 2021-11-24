3965db7a3a0ac4efefed16ac61c82fa881ca3c42
Oskar Słyk <oskar.slyk@mavoco.com>
2021-11-24 14:16:41 +0100

    Sign Up Activity

--- app/src/main/java/com/oskarro/queue/activities/SignUpActivity.kt
+++ app/src/main/java/com/oskarro/queue/activities/SignUpActivity.kt
@@ -2,11 +2,13 @@ package com.oskarro.queue.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
 import android.view.WindowManager
+import android.widget.Toast
 import com.oskarro.queue.R
 import kotlinx.android.synthetic.main.activity_sign_up.*
 
-class SignUpActivity : AppCompatActivity() {
+class SignUpActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
@@ -26,6 +28,43 @@ class SignUpActivity : AppCompatActivity() {
             actionBar.setHomeAsUpIndicator(R.drawable.ic_black_color_back_24p)
         }
         toolbar_sign_up_activity.setNavigationOnClickListener { onBackPressed() }
+
+        sign_up_page_btn.setOnClickListener {
+            registerUser()
+        }
+    }
+
+    private fun registerUser() {
+        val name: String = name_et.text.toString().trim { it <= ' ' }
+        val email: String = email_et.text.toString().trim { it <= ' ' }
+        val password: String = password_et.text.toString().trim { it <= ' ' }
+
+        if (validateForm(name, email, password)) {
+            Toast.makeText(
+                this@SignUpActivity,
+                "Now we can register a new user",
+                Toast.LENGTH_SHORT
+            ).show()
+        }
+    }
+
+    private fun validateForm(name: String, email: String, password: String): Boolean {
+        return when {
+            TextUtils.isEmpty(name) -> {
+                showErrorSnackBar("Please enter a name")
+                false
+            }
+            TextUtils.isEmpty(email) -> {
+                showErrorSnackBar("Please enter a email")
+                false
+            }
+            TextUtils.isEmpty(password) -> {
+                showErrorSnackBar("Please enter a password")
+                false
+            } else -> {
+                true
+            }
+        }
     }
 
 }
